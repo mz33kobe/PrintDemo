41b87e1adb063cdd61bc400d1fd7ac9df11080f9
mz33kobe <ndekerekiritu@gmail.com>
2021-07-04 11:25:00 +0300

    New Commit

---
+++ src/printdemo/PrintDemo.java
@@ -0,0 +1,18 @@
+package printdemo;
+
+public class PrintDemo {
+
+    public static void main(String[] args) {
+        System.out.println("");
+        System.out.println("        XX");
+        System.out.println("       *  *");
+        System.out.println("      *    *");
+        System.out.println("     * o    *");
+        System.out.println("    *    v   *");
+        System.out.println("   *   o      *");
+        System.out.println("  *         o  *");
+        System.out.println("   ************");
+        System.out.println("     ___|_|___");
+    }
+    
+}
